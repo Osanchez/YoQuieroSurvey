bbe4f2999b5fd1bb7d0197eb2c5f859500dd4a48
Omar Sanchez <osanchez@umass.edu>
2019-06-06 12:10:08 -0400

    Initial commit

---
+++ src/SurveryTaker.py
@@ -0,0 +1,39 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+class SurveyTaker:
+
+    survey_url = "https://tellthebell.com/Index.aspx?Page=1"
+
+    def __init__(self, code):
+        self.survey_code = code
+
+    def parse_survey_code(self):
+        parsed_code = self.survey_code.split("-")
+        return parsed_code
+
+    def open_survey_in_browser(self):
+        # Configure Chrome Options
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_argument("--incognito")
+        chrome_options.add_experimental_option("detach", True)
+
+        # Open the survey website
+        driver = webdriver.Chrome(ChromeDriverManager().install(), chrome_options=chrome_options)
+        driver.get(self.survey_url)
+
+        assert "Taco Bell" in driver.title
+        pass
+
+
+if __name__ == "__main__":
+    survey_code = "1234-1234-1234-1234"
+    survey_taker = SurveyTaker(survey_code)
+    survey_taker.open_survey_in_browser()
+
+
+
+
