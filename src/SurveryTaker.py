c26a0c6ef63a4db4bdfcf181ee1daac41f7e6bf2
Omar Sanchez <osanchez@umass.edu>
2019-06-07 00:40:47 -0400

    62% Completion

--- src/SurveryTaker.py
+++ src/SurveryTaker.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
@@ -73,16 +74,122 @@ class SurveyTaker:
         next_button.click()
 
         # Check if the survey code was accepted
-        if self.driver.find_element_by_class_name("Error").is_displayed():
-            print("Survey Code Failed...")
-            return False
-        else:
-            print("Survey Code Successful!")
-            return True
+        try:
+            if self.driver.find_element_by_class_name("Error").is_displayed():
+                print("Survey Code Failed...")
+        except NoSuchElementException:
+            print("Survey Code Entered Successfully!")
+
+    def select_overall_satisfaction_level(self, satisfaction_level):
+
+        print("Selecting Satisfaction Level: " + str(satisfaction_level))
+
+        # load all the satisfaction choices by class name
+        satisfaction = {
+            5: self.driver.find_element_by_class_name("Opt5"),
+            4: self.driver.find_element_by_class_name("Opt4"),
+            3: self.driver.find_element_by_class_name("Opt3"),
+            2: self.driver.find_element_by_class_name("Opt2"),
+            1: self.driver.find_element_by_class_name("Opt1")
+        }
+
+        # get the satisfaction choice element
+        satisfaction_choice = satisfaction.get(satisfaction_level)
+
+        # click the satisfaction choice element
+        satisfaction_choice.click()
+
+        # Select Next
+        next_button = self.driver.find_element_by_id("NextButton")
+        next_button.click()
+
+    def select_order_type(self):
+        print("Selecting Order Type...")
+        # Select carry out option
+        carry_out_element = self.driver.find_element_by_class_name("Opt3").find_element_by_class_name("radioButtonHolder")
+        carry_out_element.click()
+
+        # Select Next
+        next_button = self.driver.find_element_by_id("NextButton")
+        next_button.click()
+
+    def select_detailed_satisfaction(self, food_appearance, staff_friendliness, order_accuracy, store_cleanliness, order_speed, order_portion):
+        print("Selecting Detailed Experience")
+        # Get all elements for all choices
+        food_appearance_choices = {
+            5: self.driver.find_element_by_id("FNSR011000").find_element_by_class_name("Opt5"),
+            4: self.driver.find_element_by_id("FNSR011000").find_element_by_class_name("Opt4"),
+            3: self.driver.find_element_by_id("FNSR011000").find_element_by_class_name("Opt3"),
+            2: self.driver.find_element_by_id("FNSR011000").find_element_by_class_name("Opt2"),
+            1: self.driver.find_element_by_id("FNSR011000").find_element_by_class_name("Opt1")
+        }
+
+        friendliness_choices = {
+            5: self.driver.find_element_by_id("FNSR010000").find_element_by_class_name("Opt5"),
+            4: self.driver.find_element_by_id("FNSR010000").find_element_by_class_name("Opt4"),
+            3: self.driver.find_element_by_id("FNSR010000").find_element_by_class_name("Opt3"),
+            2: self.driver.find_element_by_id("FNSR010000").find_element_by_class_name("Opt2"),
+            1: self.driver.find_element_by_id("FNSR010000").find_element_by_class_name("Opt1")
+        }
+
+        accuracy_choices = {
+            5: self.driver.find_element_by_id("FNSR008000").find_element_by_class_name("Opt5"),
+            4: self.driver.find_element_by_id("FNSR008000").find_element_by_class_name("Opt4"),
+            3: self.driver.find_element_by_id("FNSR008000").find_element_by_class_name("Opt3"),
+            2: self.driver.find_element_by_id("FNSR008000").find_element_by_class_name("Opt2"),
+            1: self.driver.find_element_by_id("FNSR008000").find_element_by_class_name("Opt1")
+        }
+
+        cleanliness_choices = {
+            5: self.driver.find_element_by_id("FNSR013000").find_element_by_class_name("Opt5"),
+            4: self.driver.find_element_by_id("FNSR013000").find_element_by_class_name("Opt4"),
+            3: self.driver.find_element_by_id("FNSR013000").find_element_by_class_name("Opt3"),
+            2: self.driver.find_element_by_id("FNSR013000").find_element_by_class_name("Opt2"),
+            1: self.driver.find_element_by_id("FNSR013000").find_element_by_class_name("Opt1")
+        }
+
+        speed_choices = {
+            5: self.driver.find_element_by_id("FNSR012000").find_element_by_class_name("Opt5"),
+            4: self.driver.find_element_by_id("FNSR012000").find_element_by_class_name("Opt4"),
+            3: self.driver.find_element_by_id("FNSR012000").find_element_by_class_name("Opt3"),
+            2: self.driver.find_element_by_id("FNSR012000").find_element_by_class_name("Opt2"),
+            1: self.driver.find_element_by_id("FNSR012000").find_element_by_class_name("Opt1")
+        }
+
+        portion_choices = {
+            5: self.driver.find_element_by_id("FNSR007000").find_element_by_class_name("Opt5"),
+            4: self.driver.find_element_by_id("FNSR007000").find_element_by_class_name("Opt4"),
+            3: self.driver.find_element_by_id("FNSR007000").find_element_by_class_name("Opt3"),
+            2: self.driver.find_element_by_id("FNSR007000").find_element_by_class_name("Opt2"),
+            1: self.driver.find_element_by_id("FNSR007000").find_element_by_class_name("Opt1")
+        }
+
+        # Select all choices
+        food_appearance_choice = food_appearance_choices.get(food_appearance)
+        food_appearance_choice.click()
+
+        friendliness_choice = friendliness_choices.get(staff_friendliness)
+        friendliness_choice.click()
+
+        accuracy_choice = accuracy_choices.get(order_accuracy)
+        accuracy_choice.click()
+
+        cleanliness_choice = cleanliness_choices.get(store_cleanliness)
+        cleanliness_choice.click()
+
+        speed_choice = speed_choices.get(order_speed)
+        speed_choice.click()
+
+        portion_choice = portion_choices.get(order_portion)
+        portion_choice.click()
+
+        # Select Next
+        next_button = self.driver.find_element_by_id("NextButton")
+        next_button.click()
 
 
 if __name__ == "__main__":
-    survey_code = "1234-1234-1234-1234"
+    survey_code = "2595-5022-2551-2039"
 
     # Test Opening Survey Page
     survey_taker = SurveyTaker(survey_code)
@@ -94,6 +201,15 @@ if __name__ == "__main__":
 
     # Test Entering The Survey Code
     survey_taker.enter_survey_code()
+
+    # Test Satisfaction Level Choice
+    survey_taker.select_overall_satisfaction_level(5)
+
+    # Test Select Order Type
+    survey_taker.select_order_type()
+
+    # Test Detailed Satisfaction level Choices
+    survey_taker.select_detailed_satisfaction(5, 3, 5, 5, 4, 5)
 
 
 
